5bfc3e1fab61b9836bbdc8eadd595af7946faa09
Francisco Gonçalves <franciscocgoncalves@gmail.com>
2015-05-07 03:10:50 +0100

    fixed bug with separators

--- README.md
+++ README.md
@@ -12,11 +12,11 @@ Turn an array into a list of comma-separated values, appropriate for use in an E
 var listify = require('listify');
 
 assert(listify([1, 2]) === '1 and 2');
-assert(listify([1, 2, 3]) === '1, 2, and 3');
-assert(listify([1, 2, 3, 4]) === '1, 2, 3, and 4');
-assert(listify([1, 2, 3], { separator: '… ' }) === '1… 2… and 3');
+assert(listify([1, 2, 3]) === '1, 2 and 3');
+assert(listify([1, 2, 3, 4]) === '1, 2, 3 and 4');
+assert(listify([1, 2, 3], { separator: '… ' }) === '1… 2 and 3');
 assert(listify([1, 2, 3], { finalWord: false }) === '1, 2, 3');
-assert(listify([1, 2, 3], { separator: '… ', finalWord: 'or' }) === '1… 2… or 3');
+assert(listify([1, 2, 3], { separator: '… ', finalWord: 'or' }) === '1… 2 or 3');
 ```
 
 ## Tests
--- index.js
+++ index.js
@@ -13,17 +13,17 @@ var listify = function listify(list) {
 	var separator = options.hasOwnProperty('separator') ? options.separator : ', ';
 	var finalWord = options.hasOwnProperty('finalWord') ? options.finalWord : 'and';
 	if (finalWord.length > 0) {
-		finalWord += ' ';
+		finalWord = ' ' + finalWord + ' ';
 	}
 
 	var trimmed = list.filter(function (item) { return String(item).trim(); });
 	var str;
 	if (trimmed.length === 2 && finalWord.length > 0) {
-		str = trimmed.join(' ' + finalWord);
+		str = trimmed.join(finalWord);
 	} else if (trimmed.length < 3) {
 		str = trimmed.join(separator);
 	} else {
-		str = trimmed.slice(0, -1).concat(finalWord + trimmed[trimmed.length - 1]).join(separator);
+		str = trimmed.slice(0, -1).join(separator) + finalWord + trimmed[trimmed.length - 1];
 	}
 	return str;
 };
